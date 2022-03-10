5a0f412d9e397b2ed31932dcb96b453edc85b4b5
AlexanderKohr <94537064+AlexanderKohr@users.noreply.github.com>
2022-03-10 20:55:52 +0100

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Form, Button, Card, CardGroup, Container, Col, Row } from 'react-bootstrap';
+import axios from 'axios';
+
+import "./registration-view.scss"
 
 export function RegistrationView(props) {
     const [ username, setUsername ] = useState('');
@@ -8,26 +11,71 @@ export function RegistrationView(props) {
     const [ email, setEmail ] = useState('');
     const [ birthday, setBirthday ] = useState ('');
 
+    const [ usernameErr, setUsernameErr ] = useState('');
+    const [ passwordErr, setPasswordErr ] = useState('');
+    const [ emailErr, setEmailErr ] = useState('');
+
+    const validate = () => {
+        let isReq = true;
+        if(!username) {
+            setUsernameErr('Username Required');
+            isReq = false;
+        } else if(username.length < 2) {
+            setUsernameErr('Username must be at least 2 characters long');
+            isReq = false;
+        }
+        if(!password) {
+            setPasswordErr('Password Required');
+            isReq = false;
+        } else if(password.length < 6) {
+            setPasswordErr('Password must be at least 6 characters long');
+            isReq = false;
+        }
+        if(!email) {
+            setEmailErr('Please enter a email address');
+            isReq = false;
+        } else if(email.indexOf('@') === -1) {
+            setEmailErr('Please enter a valid email address');
+        }
+        return isReq;
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(username, password, email, birthday);
+        const isReq = validate();
 
-        props.onRegistration(username);
+        if(isReq) {
+            axios.post('https://movieanorak.herokuapp.com/users', {
+                Username: username,
+                Password: password,
+                Email: email
+            })
+            .then(response => {
+                const data = response.data;
+                console.log(data);
+                alert('Registration successful, please login');
+                window.open('/', '_self');
+            })
+            .catch(response => {
+                console.log(response);
+                alert('unable to register');
+            });
+        }
     };
 
 
 
     return (
-        <Container>
+        <Container id="registration-form">
             <Row>
                 <Col>
                     <CardGroup>
-                        <Card>
+                        <Card id="registration-card">
                             <Card.Body>
-                                <Card.Title>Please register</Card.Title>
+                                <Card.Title id="registration-card-title">Please register</Card.Title>
                                     <Form>
                                         <Form.Group>
-                                            <Form.Label>Username</Form.Label>
+                                            <Form.Label id="registration-form-label">Username</Form.Label>
                                             <Form.Control
                                                 type="text"
                                                 value={username}
@@ -35,10 +83,11 @@ export function RegistrationView(props) {
                                                 required 
                                                 placeholder="Enter a username" 
                                             />
+                                            {usernameErr && <p>{usernameErr}</p>}
                                         </Form.Group>
                                         
                                         <Form.Group>
-                                            <Form.Label>Password</Form.Label>
+                                            <Form.Label id="registration-form-label">Password</Form.Label>
                                             <Form.Control 
                                                 type="password"
                                                 value={password}
@@ -47,10 +96,11 @@ export function RegistrationView(props) {
                                                 placeholder="Enter a Password"
                                                 minLength="8" 
                                             />
+                                            {passwordErr && <p>{passwordErr}</p>}
                                         </Form.Group>
 
                                         <Form.Group>
-                                            <Form.Label>Email</Form.Label>
+                                            <Form.Label id="registration-form-label">Email</Form.Label>
                                             <Form.Control 
                                                 type="email"
                                                 value={email}
@@ -58,9 +108,10 @@ export function RegistrationView(props) {
                                                 required
                                                 placeholder="Enter your email adress" 
                                             />
+                                            {emailErr && <p>{emailErr}</p>}
                                         </Form.Group>
 
-                                        <Button variant="primary"
+                                        <Button id="register-button" variant="primary"
                                             type="submit"
                                             onClick={handleSubmit}>
                                             Register
